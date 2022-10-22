d90303fb48fb7fee9f0ddb49dcf079372b4fe1e1
Deelaka Pushpakumara <2019cs125@stu.ucsc.lk>
2022-10-22 17:09:43 +0530

    Google maps job location select

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:handyman/CustomerScreens/CustomerSubscreens/customerPostedJobs.dart';
+import 'package:handyman/CustomerScreens/CustomerSubscreens/locationPickerScreen.dart';
 import 'package:handyman/CustomerScreens/CustomerSubscreens/workerportfolio.dart';
 import 'package:handyman/CustomerScreens/homenew.dart';
 import 'package:handyman/CustomerScreens/searchScreen.dart';
@@ -46,6 +47,7 @@ class MyApp extends StatelessWidget {
         PostedJobsScreen.routeName: (ctx) => PostedJobsScreen(),
         HomeNew.routeName: (ctx) => HomeNew(),
         SearchScreen.routeName: (ctx) => SearchScreen(),
+        locationPickerScreen.routeName: (ctx) => locationPickerScreen()
       },
     );
   }
